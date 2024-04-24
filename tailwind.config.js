1791c1aa7e61efc37019b74762dcf3d81a30d2ff
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-24 14:28:27 +0100

    Update Tailwind CSS configuration to include additional file paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  darkMode: "class",
-  content: ["./**/*.tsx"],
+  darkMode: "media",
+  content: ["./src/**/*.{js,ts,jsx,tsx}", "./**/*.{ts,tsx}"],
   plugins: [require("@tailwindcss/forms")]
 };
