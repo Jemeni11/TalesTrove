a450f8403f9799ae85c7bea9e6a1f687be84c873
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-24 14:31:16 +0100

    Refactor adapters/index.ts to export all adapter functions

---
+++ src/adapters/index.ts
@@ -0,0 +1,9 @@
+import getArchiveOfOurOwnData from "./archiveofourown";
+import getFanFictionNetStoryData from "./fanfictiondotnet";
+import getQuestionableQuestingData from "./questionablequesting";
+
+export {
+  getArchiveOfOurOwnData,
+  getFanFictionNetStoryData,
+  getQuestionableQuestingData
+};
