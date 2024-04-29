33baaf58b8cc9ff09a1e922814312867a5524e03
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-04-29 18:01:19 +0100

    Add types.ts file with workObjectType and authorType definitions

---
+++ src/types.ts
@@ -0,0 +1,12 @@
+export type workObjectType = {
+  id: string;
+  title: string;
+  link: string;
+  authorName: string[];
+  authorLink: string[];
+};
+
+export type authorType = {
+  name: string;
+  link: string;
+};
