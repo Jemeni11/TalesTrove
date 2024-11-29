6fa067d849dd3f1afd1d64e7b6852cb88b7a5fa2
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-11-29 23:22:48 +0100

    Add new types for external data sources and file formats
    - Introduced `QQDataType`, `FFProcessedStoryData`, `FFData`, and `SubscriptionResult` types for standardized handling of external data.
    - Added `sitesDataType` and related types for configuration management of supported sites.
    - Expanded `fileFormatType` to include options for JSON, TXT, CSV, HTML, and Bookmarks HTML.

--- src/types.ts
+++ src/types.ts
@@ -10,3 +10,66 @@ export type authorType = {
   name: string;
   link: string;
 };
+
+export type QQDataType = {
+  storyLink: string;
+  storyName: string;
+  authorLink: string;
+  authorName: string;
+};
+
+export type FFProcessedStoryData = {
+  authorLink: string;
+  authorName: string;
+  storyLink: string;
+  storyTitle: string;
+  dateCreated: string;
+  dateUpdated: string;
+};
+
+export type SubscriptionResult = {
+  authors?: authorType[];
+  works?: workObjectType[];
+  series?: workObjectType[];
+};
+
+export type FFData = {
+  storyData: {
+    storyLink: string;
+    storyTitle: string;
+  };
+  authorData: {
+    authorLink: string;
+    authorName: string;
+  };
+  dateCreated: string;
+  dateUpdated: string;
+};
+
+export type sitesDataType = {
+  fanfiction: {
+    favorites: boolean;
+    following: boolean;
+  };
+  archiveOfOurOwn: {
+    work: boolean;
+    series: boolean;
+    author: boolean;
+    username: string;
+  };
+  questionableQuesting: {
+    following: boolean;
+  };
+};
+
+export type sitesDataTypeKey = keyof sitesDataType;
+
+export type fileFormatType = {
+  json: boolean;
+  txt: boolean;
+  csv: boolean;
+  html: boolean;
+  bookmarksHtml: boolean;
+};
+
+export type fileFormatTypeKey = keyof fileFormatType;
