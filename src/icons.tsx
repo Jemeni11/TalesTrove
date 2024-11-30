dc4d0bf46c157c25f9bc75f7fa3d76e45614fcb8
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-11-30 01:38:34 +0100

    feat: update icon exports to include new icons (Users, User, FileText, Download, ChevronDown, BookOpen, Bookmark)

--- src/icons.tsx
+++ src/icons.tsx
@@ -1,12 +1,13 @@
-import React from "react";
+import type { SVGProps } from "react";
 
-function JSONIcon() {
+export function JSONIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 32 32">
+      viewBox="0 0 32 32"
+      {...props}>
       <path
         fill="currentColor"
         d="M31 11v10h-2l-2-6v6h-2V11h2l2 6v-6zm-9.666 10h-2.667A1.67 1.67 0 0 1 17 19.334v-6.667A1.67 1.67 0 0 1 18.666 11h2.667A1.67 1.67 0 0 1 23 12.666v6.667A1.67 1.67 0 0 1 21.334 21M19 19h2v-6h-2zm-5.666 2H9v-2h4v-2h-2a2 2 0 0 1-2-2v-2.334A1.67 1.67 0 0 1 10.666 11H15v2h-4v2h2a2 2 0 0 1 2 2v2.334A1.67 1.67 0 0 1 13.334 21m-8 0H2.667A1.67 1.67 0 0 1 1 19.334V17h2v2h2v-8h2v8.334A1.67 1.67 0 0 1 5.334 21"></path>
@@ -14,13 +15,14 @@ function JSONIcon() {
   );
 }
 
-function CSVIcon() {
+export function CSVIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 32 32">
+      viewBox="0 0 32 32"
+      {...props}>
       <path
         fill="currentColor"
         d="m28 9l-2 13l-2-13h-2l2.516 14h2.968L30 9zM18 23h-6v-2h6v-4h-4a2 2 0 0 1-2-2v-4a2 2 0 0 1 2-2h6v2h-6v4h4a2 2 0 0 1 2 2v4a2 2 0 0 1-2 2m-8 0H4a2 2 0 0 1-2-2V11a2 2 0 0 1 2-2h6v2H4v10h6z"></path>
@@ -28,13 +30,14 @@ function CSVIcon() {
   );
 }
 
-function TXTIcon() {
+export function TXTIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 32 32">
+      viewBox="0 0 32 32"
+      {...props}>
       <path
         fill="currentColor"
         d="M21 11h3v12h2V11h3V9h-8zm-1-2h-2l-2 6l-2-6h-2l2.75 7L12 23h2l2-6l2 6h2l-2.75-7zM3 11h3v12h2V11h3V9H3z"></path>
@@ -42,13 +45,14 @@ function TXTIcon() {
   );
 }
 
-function HTMLIcon() {
+export function HTMLIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 32 32">
+      viewBox="0 0 32 32"
+      {...props}>
       <path
         fill="currentColor"
         d="M28 19v-8h-2v10h6v-2zm-4-8h-2l-1.5 4l-1.5-4h-2v10h2v-7l1.5 4l1.5-4v7h2zM9 13h2v8h2v-8h2v-2H9zm-4-2v4H2v-4H0v10h2v-4h3v4h2V11z"></path>
@@ -56,13 +60,14 @@ function HTMLIcon() {
   );
 }
 
-function BookmarksHTMLIcon() {
+export function BookmarksHTMLIcon(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       width="1em"
       height="1em"
-      viewBox="0 0 32 32">
+      viewBox="0 0 32 32"
+      {...props}>
       <path
         fill="currentColor"
         d="M24 16v10.752l-7.096-3.59l-.904-.457l-.9.456L8 26.748V4h10V2H8a2 2 0 0 0-2 2v26l10-5.054L26 30V16Z"></path>
@@ -71,4 +76,142 @@ function BookmarksHTMLIcon() {
   );
 }
 
-export { JSONIcon, CSVIcon, TXTIcon, HTMLIcon, BookmarksHTMLIcon };
+export function Users(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}>
+        <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path>
+        <circle cx={9} cy={7} r={4}></circle>
+        <path d="M22 21v-2a4 4 0 0 0-3-3.87m-3-12a4 4 0 0 1 0 7.75"></path>
+      </g>
+    </svg>
+  );
+}
+
+export function User(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}>
+        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+        <circle cx={12} cy={7} r={4}></circle>
+      </g>
+    </svg>
+  );
+}
+
+export function FileText(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <g
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}>
+        <path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"></path>
+        <path d="M14 2v4a2 2 0 0 0 2 2h4M10 9H8m8 4H8m8 4H8"></path>
+      </g>
+    </svg>
+  );
+}
+
+export function Download(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4m4-5l5 5l5-5m-5 5V3"></path>
+    </svg>
+  );
+}
+
+export function ChevronDown(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="m6 9l6 6l6-6"></path>
+    </svg>
+  );
+}
+
+export function BookOpen(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M12 7v14m-9-3a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4a4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3a3 3 0 0 0-3-3z"></path>
+    </svg>
+  );
+}
+
+export function Bookmark(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+      {...props}>
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="m19 21l-7-4l-7 4V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>
+    </svg>
+  );
+}
