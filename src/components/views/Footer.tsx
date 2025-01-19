2582a343a98802215b31fb2d4c5a1473bb157097
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-01-19 12:20:12 +0100

    feat(footer): add link to GitHub repository
    - Added a link to the TalesTrove GitHub repository for users to explore the code.
    - Enhanced the appearance of the existing author link with an underline for better visibility.
    - Structured the footer with a clear hierarchy, separating the author credit and repository link.

--- src/components/views/Footer.tsx
+++ src/components/views/Footer.tsx
@@ -1,16 +1,21 @@
 export default function Footer() {
   return (
-    <div className="w-full text-center my-4">
-      <p className="text-sm text-gray-700">
+    <div className="text-sm w-full text-center my-4">
+      <p className="text-gray-700">
         Made with ❤️ by{" "}
         <a
           href="https://www.github.com/Jemeni11"
           target="_blank"
           rel="noreferrer"
-          className="text-blue-500">
+          className="text-blue-500 underline underline-offset-2">
           Jemeni
         </a>
       </p>
+      <a
+        href="https://github.com/Jemeni11/Tales-Trove"
+        className="mt-4 text-xs">
+        <strong>Explore the GitHub repo »</strong>
+      </a>
     </div>
   );
 }
