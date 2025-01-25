30efccab24cbc8022f9da8e0a93cb64bd59acaf8
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-01-25 12:31:27 +0100

    feat(utils): add `saveLinksOnlyTXTFile` to exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,12 +4,14 @@ import customError from "./customError";
 import handleExport from "./handleExport";
 import saveHTMLFile from "./html";
 import saveJSONFile from "./json";
+import saveLinksOnlyTXTFile from "./linksOnly";
 import saveTXTFile from "./txt";
 
 export {
   saveCSVFile,
   saveJSONFile,
   saveHTMLFile,
+  saveLinksOnlyTXTFile,
   saveBookmarkHTMLFile,
   saveTXTFile,
   handleExport,
