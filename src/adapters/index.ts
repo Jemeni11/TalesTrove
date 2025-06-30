ac6d5a87f25d4cbd5a86c65ac64d216d323d00aa
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-30 16:51:47 +0100

    refactor(adapters): consolidate XenForo adapters into single module

--- src/adapters/index.ts
+++ src/adapters/index.ts
@@ -1,14 +1,10 @@
 import getArchiveOfOurOwnData from "./archiveofourown";
 import { getFFFavoritesData, getFFFollowingData } from "./fanfictiondotnet";
-import getQuestionableQuestingData from "./questionablequesting";
-import getSpaceBattlesData from "./spacebattles";
-import getSufficientVelocityData from "./sufficientvelocity";
+import getXenForoData from "./xenforo";
 
 export {
   getArchiveOfOurOwnData,
   getFFFavoritesData,
   getFFFollowingData,
-  getQuestionableQuestingData,
-  getSpaceBattlesData,
-  getSufficientVelocityData
+  getXenForoData
 };
