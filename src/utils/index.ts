1a331934a554786209363ce9530db31ef0ed1f7e
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-06-30 13:16:40 +0100

    refactor(utils): reorganize file structure and exports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,11 +1,13 @@
-import saveBookmarkHTMLFile from "./bookmark_html";
-import saveCSVFile from "./csv";
 import customError from "./customError";
+import delay from "./delay";
+import saveBookmarkHTMLFile from "./file_formats/bookmark_html";
+import saveCSVFile from "./file_formats/csv";
+import saveHTMLFile from "./file_formats/html";
+import saveJSONFile from "./file_formats/json";
+import saveLinksOnlyTXTFile from "./file_formats/linksOnly";
+import saveTXTFile from "./file_formats/txt";
+import getDocument from "./getDocument";
 import handleExport from "./handleExport";
-import saveHTMLFile from "./html";
-import saveJSONFile from "./json";
-import saveLinksOnlyTXTFile from "./linksOnly";
-import saveTXTFile from "./txt";
 
 export {
   saveCSVFile,
@@ -15,5 +17,7 @@ export {
   saveBookmarkHTMLFile,
   saveTXTFile,
   handleExport,
-  customError
+  customError,
+  getDocument,
+  delay
 };
