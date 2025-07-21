50f18d92a0dd689cdbeba28f23b271f3baebf912
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-21 17:27:17 +0100

    feat(background): open internal tab page on extension icon click

--- src/background/index.ts
+++ src/background/index.ts
@@ -1,1 +1,13 @@
+export {};
 
+const openTab = () => {
+  chrome.tabs.create({
+    url: chrome.runtime.getURL("tabs/talestrove.html")
+  });
+};
+
+if (chrome.action) {
+  chrome.action.onClicked.addListener(openTab);
+} else if (chrome.browserAction) {
+  chrome.browserAction.onClicked.addListener(openTab);
+}
