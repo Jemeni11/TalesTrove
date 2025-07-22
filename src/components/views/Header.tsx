5a141c97ffaf459615df263d631dc2b5e481873a
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-22 22:01:17 +0100

    style: tweak Header padding to match new layout spacing

--- src/components/views/Header.tsx
+++ src/components/views/Header.tsx
@@ -2,7 +2,7 @@ import logo from "data-base64:../../../assets/icon.png";
 
 export default function Header() {
   return (
-    <div className="p-4 flex items-center gap-x-4">
+    <div className="py-4 flex items-center gap-x-4">
       <img src={logo} alt="logo" width={32} height={32} />
       <h1 className="text-2xl font-bold text-black">TalesTrove</h1>
     </div>
