4e869d2921ac900c6c4aea8098876c5cfb24e977
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2025-07-24 08:03:23 +0100

    feat(footer): add donation links and animate heart

--- src/components/views/Footer.tsx
+++ src/components/views/Footer.tsx
@@ -1,8 +1,10 @@
+import { BuyMeACoffee, GitHubSponsors } from "~icons";
+
 export default function Footer() {
   return (
-    <div className="text-sm w-full text-center my-4">
+    <div className="text-sm w-full space-y-2 text-center my-4">
       <p className="text-gray-700">
-        Made with ❤️ by{" "}
+        Made with <span className="animate-pulse">❤️</span> by{" "}
         <a
           href="https://www.github.com/Jemeni11"
           target="_blank"
@@ -15,9 +17,23 @@ export default function Footer() {
         href="https://github.com/Jemeni11/Tales-Trove"
         target="_blank"
         rel="noreferrer"
-        className="mt-4 text-xs">
-        <strong>Check out the Manual »</strong>
+        className="text-xs">
+        <strong>Check out the Repo »</strong>
       </a>
+      <div className="flex w-full justify-center items-center gap-x-6 sm:gap-x-4">
+        <a
+          href="https://www.buymeacoffee.com/jemeni11"
+          target="_blank"
+          rel="noopener noreferrer">
+          <BuyMeACoffee />
+        </a>
+        <a
+          href="https://github.com/sponsors/Jemeni11/"
+          target="_blank"
+          rel="noopener noreferrer">
+          <GitHubSponsors />
+        </a>
+      </div>
     </div>
   );
 }
